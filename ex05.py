9a0aaf1e2c675aa1a9965e970e7bebc762df960b
yangq7 <yangqinglu007@gmail.com>
2016-03-24 18:50:58 -0400

    This is my f script

--- ex05.py
+++ ex05.py
@@ -1,2 +1,6 @@
 # Exercise 5
 ## Objective: create a function combine(word1,word2); when run, the function should return a portmanteau of the inputted parameters
+def combine(word1,word2):
+	return word1word2
+
+         
